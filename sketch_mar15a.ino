cf0db80b0ad6cc0ef973c3ee202ac4466cc6b10b
Yudy9924 <duysas99@gmail.com>
2025-04-16 10:01:39 +0700

    Add files via upload

--- sketch_mar15a.ino
+++ sketch_mar15a.ino
@@ -7,8 +7,6 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <PZEM004Tv30.h>
-#include <SinricPro.h>
-#include <SinricProSwitch.h>
 
 #define DHTPIN 4
 #define DHTTYPE DHT11
@@ -19,29 +17,8 @@ const char* password = "00001111";
 const char* serverName = "http://192.168.119.183:5000/data";
 const char* serverURL = "http://192.168.119.183:5000/power-data";  
 
-#define APP_KEY     "8e10eb5d-2f08-4317-908b-322a540c6239"  
-#define APP_SECRET  "07f2aa77-e000-4d17-aed1-c6af2b2b8e37-51b7fd9f-232a-4fdc-83a8-833818b01bf5"
-#define DEVICE_ID_1 "67d13b264dee339ca79b6f80"  // Đèn 1
-#define DEVICE_ID_2 "67d13b566066f22be0656db5"  // Đèn 2
-
-// ✅ Định nghĩa chân Relay
-#define RELAY_1 33
-#define RELAY_2 32
-
 HardwareSerial pzemSerial(1);
 PZEM004Tv30 pzem(pzemSerial, 16, 17);
-
-bool onPowerState1(const String &deviceId, bool &state) {
-    digitalWrite(RELAY_1, state ? HIGH : LOW);
-    Serial.printf("Light 1 %s\n", state ? "ON" : "OFF");
-    return true;
-}
-
-bool onPowerState2(const String &deviceId, bool &state) {
-    digitalWrite(RELAY_2, state ? HIGH : LOW);
-    Serial.printf("Light 2 %s\n", state ? "ON" : "OFF");
-    return true;
-}
 
 void setup() {
     Serial.begin(115200);
@@ -52,20 +29,6 @@ void setup() {
     }
     Serial.println("Connected to WiFi!");
     dht.begin();
-
-    pinMode(RELAY_1, OUTPUT);
-    pinMode(RELAY_2, OUTPUT);
-    digitalWrite(RELAY_1, LOW);
-    digitalWrite(RELAY_2, LOW);
-
-    // ✅ Cấu hình thiết bị trên Sinric Pro
-    SinricProSwitch &myLight1 = SinricPro[DEVICE_ID_1];
-    myLight1.onPowerState(onPowerState1);
-
-    SinricProSwitch &myLight2 = SinricPro[DEVICE_ID_2];
-    myLight2.onPowerState(onPowerState2);
-
-    SinricPro.begin(APP_KEY, APP_SECRET);
 }
 
 void loop() {
@@ -114,9 +77,7 @@ void loop() {
 
     sendHumidity();
     sendToServer(voltage, current, power, energy, frequency, pf);
-    
-  SinricPro.handle();
-  
+
     delay(5000);
 }
 
@@ -185,7 +146,6 @@ void sendToServer(float voltage, float current, float power, float energy, float
 
     http.end();
 }
-
 
 void checkWiFiConnection() {
     if (WiFi.status() != WL_CONNECTED) {
